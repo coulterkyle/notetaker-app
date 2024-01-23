c961df19b67e832dc46dc354f36103c13566d67e
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2024-01-22 19:23:39 -0500

    fixed delete button

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -89,11 +89,11 @@ const handleNoteSave = () => {
 const handleNoteDelete = (e) => {
   // Prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
-
+//note_id
   const note = e.target;
-  const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
-
-  if (activeNote.id === noteId) {
+  const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).note_id;
+//note_id
+  if (activeNote.note_id === noteId) {
     activeNote = {};
   }
 
